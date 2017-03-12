8f678fd19f44739693d1cf8287a503683e604d3f
adamzfc <adamzfc@gmail.com>
2017-03-12 20:48:27 +0800

    first commit

---
+++ byterun.py
@@ -0,0 +1,144 @@
+#-*- coding:utf-8 -*-
+import collections
+import operator
+import dis
+import sys
+import types
+import inspect
+
+class VirtualMachineError(Exception):
+    pass
+
+
+class VirtualMachine(object):
+    def __init__(self):
+        self.frames = []
+        self.frame = None
+        self.return_value = None
+        self.last_exception = None
+
+    def run_code(self, code, global_names=None, local_names=None):
+        frame = self.make_frame(code, global_names=global_names,
+                    local_names=local_names)
+        self.run_frame(frame)
+
+    def make_frame(self, code, callargs={}, global_names=None, local_names=None):
+        if global_names is not None:
+            global_names = global_names
+            if local_names is None:
+                local_names = global_names
+        elif self.frames:
+            global_names = self.frame.global_names
+            local_names = {}
+        else:
+            global_names = local_names = {
+                    '__builtins__': __builtins__,
+                    '__name__': '__main__',
+                    '__doc__': None,
+                    '__package__': None,
+            }
+        local_names.update(callargs)
+        frame = Frame(code, global_names, local_names, self.frame)
+        return frame
+
+    def push_frame(self, frame):
+        self.frames.append(frame)
+        self.frame = frame
+
+    def pop_frame(self):
+        self.frames.pop()
+        if self.frames:
+            self.frame = self.frames[-1]
+        else:
+            self.frame = None
+
+    def run_frame(self, frame):
+        pass
+
+    def top(self):
+        return self.frame.stack[-1]
+
+    def pop(self):
+        return self.frame.stack.pop()
+
+    def push(self, *vals):
+        self.frame.stack.extend(vals)
+
+    def popn(self, n):
+        if n:
+            ret = self.frame.stack[-n:]
+            self.frame.stack[-n:] = []
+            return ret
+        else:
+            return []
+
+    def parse_byte_and_args(self):
+        f = self.frame
+        opoffset = f.last_instruction
+        byteCode = ord(f.code_obj.co_code[opoffset])
+        f.last_instruction += 1
+        byte_name = dis.opname[byteCode]
+        if byteCode >= dis.HAVE_ARGUMENT:
+            arg = f.code_obj.co_code[f.last_instruction:f.last_instruction+2]
+            f.last_instruction += 2
+            arg_val = ord(arg[0]) + (ord(arg[1]) * 256)
+            if byteCode in dis.hasconst:
+                arg = f.code_obj.co_consts[arg_val]
+            elif byteCode in dis.hasname:
+                arg = f.code_obj.co_names[arg_val]
+
+
+
+class Frame(object):
+    def __init__(self, code_obj, global_names, local_names, prev_frame):
+        self.code_obj = code_obj
+        self.f_globals = global_names
+        self.f_locals = local_names
+        self.prev_frame = prev_frame
+        self.stack = []
+        self.block_stack = []
+        if prev_frame:
+            self.builtin_names = prev_frame.builtin_names
+        else:
+            self.builtin_names = local_names['__builtins__']
+            if hasattr(self.builtin_names, '__dict__'):
+                self.builtin_names = self.builtin_names.__dict__
+        self.last_instruction = 0
+
+
+class Function(object):
+    __slot__ = [
+            'func_code', 'func_name', 'func_defaults', 'func_globals',
+            'func_locals', 'func_dict', 'func_closure',
+            '__name__', '__dict__', '__doc__',
+            '_vm', '_func',
+            ]
+
+    def __init__(self, name, code, globs, defaults, closure, vm):
+        self._vm = vm
+        self.func_code = code
+        self.func_name = self.__name__ = name or code.co_name
+        self.func_defaluts = tuple(defaluts)
+        self.func_globals = globs
+        self.func_locals = self._vm.frame.f_locals
+        self.__dict__ = {}
+        self.func_closure = closure
+        self.__doc__ = code.co_consts[0] if code.co_consts else None
+        kw = {
+                'argdefs': self.func_defaults,
+            }
+        if closure:
+            kw['closure'] = tuple(make_cell(0) for _ in closure)
+        self._func = types.FunctionType(code, globs, **kw)
+
+    def __call__(self, *args, **kwargs):
+        callargs = inspect.getcallargs(self,_func, *args, **kwargs)
+        frame = self._vm.make_frame(
+                self.func_code, callargs, self.func_globals, {}
+            )
+        return self._vm.run_frame(frame)
+
+
+def make_cell(value):
+    fn = (lambda x: lambda: x)(value)
+    return fn.__closure__[0]
